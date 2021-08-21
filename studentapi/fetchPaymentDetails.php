26f671c2426abd6552ce83365c70ac3288d4b80f
KrishnaWahi <krishna@brontobyteanalytics.com>
2021-08-21 12:40:11 +0530

    FetchPayment Response Fix.

--- studentapi/fetchPaymentDetails.php
+++ studentapi/fetchPaymentDetails.php
@@ -9,11 +9,13 @@ $con = $dbConnection->getConnection();
 
 $registrationNo = $_GET['registrationNo'];
 $sql_query = "SELECT * from payment WHERE registrationNo='$registrationNo'";
+$result = mysqli_query($con, $sql_query);
 
-if ($result = mysqli_query($con, $sql_query)) {
+if (mysqli_num_rows($result) > 0) {
     $response = array(mysqli_fetch_assoc($result));
     echo json_encode($response);
 } else {
+    echo json_encode("");
     header(' 500 Internal Server Error', true, 500);
 }
 $dbConnection->closeConnection();
