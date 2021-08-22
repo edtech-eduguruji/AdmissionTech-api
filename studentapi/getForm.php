d04952f35e044678108390137ad9bdc2dba2f894
KrishnaWahi <krishna@brontobyteanalytics.com>
2021-08-22 19:00:58 +0530

    Admin Side Changes.

--- studentapi/getForm.php
+++ studentapi/getForm.php
@@ -7,7 +7,17 @@ include_once('../utils.php');
 $dbConnection = new DBConnection($db);
 $con = $dbConnection->getConnection();
 
-$registrationNo = $_GET['registrationNo'];
+if (isset($_GET['registrationNo'])) {
+    $registrationNo = $_GET['registrationNo'];
+    $WHERE = "WHERE basic_details.registrationNo='$registrationNo'";
+    $response = null;
+} else {
+    $limit = $_GET['limit'];
+    $offset = $_GET['offset'];
+    $WHERE = "WHERE payment = '1' AND submitted = '1' LIMIT $limit OFFSET $offset";
+    $response = array();
+}
+
 
 $sql_query = "SELECT * from basic_details 
 INNER JOIN advanced_details ON basic_details.registrationNo = advanced_details.registrationNo 
@@ -15,16 +25,20 @@ INNER JOIN academic_details ON basic_details.registrationNo = academic_details.r
 INNER JOIN faculty_course_details ON basic_details.registrationNo = faculty_course_details.registrationNo
 INNER JOIN documents ON basic_details.registrationNo = documents.registrationNo 
 INNER JOIN merit_details ON basic_details.registrationNo = merit_details.registrationNo 
-WHERE basic_details.registrationNo='$registrationNo'";
+$WHERE";
 
 // -- INNER JOIN payment ON basic_details.registrationNo = payment.registrationNo 
 
 $result = mysqli_query($con, $sql_query);
-$response = null;
+
 
 if ($result) {
     while ($row = mysqli_fetch_assoc($result)) {
-        $response = $row;
+        if (isset($_GET['registrationNo'])) {
+            $response = $row;
+        } else {
+            array_push($response, $row);
+        }
     }
 } else {
     header(' 500 Internal Server Error', true, 500);
