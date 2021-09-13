a0a1dcbe99274cbef54142f353a457de7e610b0f
Kushal Jain <kushal.jain63@gmail.com>
2021-09-13 12:13:36 +0530

    update fetch payment details

--- studentapi/fetchPaymentDetails.php
+++ studentapi/fetchPaymentDetails.php
@@ -7,18 +7,43 @@ include_once('../utils.php');
 $dbConnection = new DBConnection($db);
 $con = $dbConnection->getConnection();
 
-$registrationNo = $_GET['registrationNo'];
 
-$sql_query = "SELECT payment.*, basic_details.submitted,basic_details.payment,users_info.user_id,users_info.user_name,
-users_info.password,users_info.role,users_info.active FROM users_info 
-INNER JOIN basic_details ON users_info.user_id = basic_details.registrationNo 
-INNER JOIN payment ON payment.registrationNo = basic_details.registrationNo 
-WHERE payment.registrationNo='$registrationNo' and AuthStatusCode='0300' and basic_details.payment='1' ";
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 
-$result = mysqli_query($con, $sql_query);
+switch ($requestMethod) {
+    case "GET":
+        $registrationNo = $_GET['registrationNo'];
 
-if (mysqli_num_rows($result) > 0) {
-    $response = (mysqli_fetch_assoc($result));
-    echo createToken($response);
+        $sql_query = "SELECT payment.*, basic_details.submitted,basic_details.payment,users_info.user_id,users_info.user_name,
+        users_info.password,users_info.role,users_info.active FROM users_info 
+        INNER JOIN basic_details ON users_info.user_id = basic_details.registrationNo 
+        INNER JOIN payment ON payment.registrationNo = basic_details.registrationNo 
+        WHERE payment.registrationNo='$registrationNo' and AuthStatusCode='0300' and basic_details.payment='1' ";
+
+        $result = mysqli_query($con, $sql_query);
+
+        if (mysqli_num_rows($result) > 0) {
+            $response = (mysqli_fetch_assoc($result));
+            echo createToken($response);
+        }
+        $dbConnection->closeConnection();
+        break;
+
+    case "POST":
+        $registrationNo = $_GET['registrationNo'];
+
+        $sql_query = "SELECT payment.*, basic_details.submitted,basic_details.payment,users_info.user_id,users_info.user_name,
+        users_info.password,users_info.role,users_info.active FROM users_info 
+        INNER JOIN basic_details ON users_info.user_id = basic_details.registrationNo 
+        INNER JOIN payment ON payment.registrationNo = basic_details.registrationNo 
+        WHERE payment.registrationNo='$registrationNo' ";
+
+        $result = mysqli_query($con, $sql_query);
+
+        if (mysqli_num_rows($result) > 0) {
+            $response = (mysqli_fetch_assoc($result));
+            echo createToken($response);
+        }
+        $dbConnection->closeConnection();
+        break;
 }
-$dbConnection->closeConnection();
