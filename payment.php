6073498100f5d39976d17930e682e986f7ed6329
KrishnaWahi <krishnawahi69@gmail.com>
2022-07-05 22:49:00 +0530

    "clg_id" Changes.

--- payment.php
+++ payment.php
@@ -11,9 +11,9 @@ error_log("PI: " . $data);
 $myArray = explode('|', $data);
 $countVal = count($myArray);
 
-$billdesk_checksum = $myArray[$countVal-1];
+$billdesk_checksum = $myArray[$countVal - 1];
 $billdesk_checksum = trim($billdesk_checksum);
-array_splice($myArray, $countVal-1, $countVal);
+array_splice($myArray, $countVal - 1, $countVal);
 $str = implode('|', $myArray);
 //echo $str;
 
@@ -25,39 +25,42 @@ $gen_checksum = trim($gen_checksum);
 // us|SettlementType|AdditionalInfo1|AdditionalInfo2|AdditionalInfo3|AdditionalInfo4|Addition
 // alInfo5|AdditionalInfo6|AdditionalInfo7|ErrorStatus|ErrorDescription|CheckSum
 
-$transTypeArray = array('01'=>'Netbanking',
-    '02'=>'Credit Card',   
-    '03'=>'Debit Card',
-    '04'=>'Cash Card',
-    '05'=>'Mobile Wallet',
-    '06'=>'IMPS',
-    '07'=>'Reward Points',
-    '08'=>'Rupay',
-    '09'=>'Others',
-    '10'=>'UPI');    
-    
-    // Type Transaction
-    $authStatusArray = array( 
-    '0300'=>'Success',
-    '0399'=>'Failure',
-    'NA'=>'Failure',
-    '0002'=>'Pending',
-    '0001'=>'Failure');
+$transTypeArray = array(
+    '01' => 'Netbanking',
+    '02' => 'Credit Card',
+    '03' => 'Debit Card',
+    '04' => 'Cash Card',
+    '05' => 'Mobile Wallet',
+    '06' => 'IMPS',
+    '07' => 'Reward Points',
+    '08' => 'Rupay',
+    '09' => 'Others',
+    '10' => 'UPI'
+);
 
-    $UniqueTxnID = $myArray[1];
-    $TxnReferenceNo = $myArray[2];
-    $BankReferenceNo = $myArray[3];
-    $TxnAmount = ltrim($myArray[4], "0");
-    $BankID = $myArray[5];
-    $BIN = $myArray[6];
-    $TxnTypeCode = $myArray[7];
-    $TxnDate = $myArray[13];
-    $AuthStatusCode = $myArray[14];
-    $PaymentFacultyId = $myArray[16];
-    $registrationNo = $myArray[17];
-    $courseFee = $myArray[18];
-    $errorCode = $myArray[22];
-    $errorDescription = $myArray[23];
+// Type Transaction
+$authStatusArray = array(
+    '0300' => 'Success',
+    '0399' => 'Failure',
+    'NA' => 'Failure',
+    '0002' => 'Pending',
+    '0001' => 'Failure'
+);
+
+$UniqueTxnID = $myArray[1];
+$TxnReferenceNo = $myArray[2];
+$BankReferenceNo = $myArray[3];
+$TxnAmount = ltrim($myArray[4], "0");
+$BankID = $myArray[5];
+$BIN = $myArray[6];
+$TxnTypeCode = $myArray[7];
+$TxnDate = $myArray[13];
+$AuthStatusCode = $myArray[14];
+$PaymentFacultyId = $myArray[16];
+$registrationNo = $myArray[17];
+$courseFee = $myArray[18];
+$errorCode = $myArray[22];
+$errorDescription = $myArray[23];
 
 //Time entry
 date_default_timezone_set('Asia/Kolkata');
@@ -66,13 +69,13 @@ $payment = '0';
 $admissionYear = null;
 $submitted = '0';
 
-if($billdesk_checksum==$gen_checksum) {
-    if($AuthStatusCode == "0300") {
+if ($billdesk_checksum == $gen_checksum) {
+    if ($AuthStatusCode == "0300") {
         $payment = '1';
 
         $dbConnection = new DBConnection($db);
         $con = $dbConnection->getConnection();
-        
+
         $query = "SELECT basic_details.submitted, basic_details.payment, basic_details.courseFee, faculty_course_details.admissionYear 
         FROM basic_details INNER JOIN faculty_course_details ON faculty_course_details.registrationNo = basic_details.registrationNo 
         WHERE basic_details.registrationNo = '$registrationNo' ";
@@ -107,17 +110,15 @@ $user = array(
     'PaymentFacultyId' => $PaymentFacultyId,
     'errorCode' => $errorCode,
     'errorDescription' => $errorDescription,
-
     'registrationNo' => $registrationNo,
     'payment' => $payment,
     'submitted' => $submitted,
     'role' => 'STUDENT',
     'active' => '1',
     'user_name' => $registrationNo,
-    'user_id' => $registrationNo,
-
+    'clg_id' => $registrationNo,
     'courseFee' => $courseFee,
-    'admissionYear'=> $admissionYear
+    'admissionYear' => $admissionYear
 );
 
 $jwt = createToken($user);
